a8258a799ce5d9bc5c856017d1af9b364301d55b
C____J <rlatlswo13@gmail.com>
2022-04-15 18:46:39 +0900

    Add files via upload

---
+++ gamePlay.js
@@ -0,0 +1,226 @@
+let alreadyPlay = 'false'
+const cactus = document.getElementById('cactus')
+const playerBox = document.getElementById('player')
+const Cac_ctx = cactus.getContext('2d')
+const Player_ctx = playerBox.getContext('2d')
+const scoreBox = document.getElementById('score')
+const lifeBox = document.getElementById('life')
+const levelBox = document.getElementById('level')
+const game_play_container = document.getElementById('game-play')
+let gameOverBox = document.getElementsByClassName('game-over')[0]
+const[mainBgm,eatBgm,swingBgm,hurtBgm,missFoodBgm,levelUpBgm,gameEndBgm] = document.querySelectorAll('audio')
+let player;
+
+class Cactus{
+    constructor(x,y,width,height,img,eat){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.img=img
+        this.eat=eat
+    }
+    create(){
+        Cac_ctx.fillStyle = '#ffffff00'
+        Cac_ctx.fillRect (this.x,this.y, this.width, this.height);
+        Cac_ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    }
+}
+class Doll{
+    constructor(x,y,width,height,img){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.img=img
+    }
+    create(){
+        Player_ctx.fillStyle = '#ffffff00'
+        Player_ctx.fillRect (this.x,this.y, this.width, this.height);
+        Player_ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    }
+}
+const playerImg = new Image()
+playerImg.src='./Image/player.jpeg'
+playerImg.onload=function(){
+    player = new Doll(150,0,100,100,playerImg)
+}
+
+export function gamePlay(){
+    /*
+    지금드는 해결생각 -> player를 전역으로,
+    */
+    //player 생성 -> 이미지 로드되면 캔버스에 create
+    //데이터상 필요한부분
+    const foodImg=document.getElementsByClassName('img')
+    let timer =0;
+    let cactusArray=[];
+    let score = 0;
+    let level = 1;
+    let speed = 10;
+    let life=3;
+    lifeBox.textContent=`LIFE:${life}`;
+    levelBox.textContent=`LEVEL:${level}`;
+    scoreBox.textContent=`SCORE:${score}`;
+    
+    /* ------------------------------ */ 
+    if(alreadyPlay === 'false'){
+        window.addEventListener('keydown',(e)=>{
+                movePlayer(e.key)
+                alreadyPlay = 'true'
+        })
+    }
+
+    //기준 height는 일단 690px
+    const gpc_width = window.getComputedStyle(game_play_container).width
+    const gpc_height = window.getComputedStyle(game_play_container).height
+    cactus.width = Number(gpc_width.slice(0,gpc_width.length-2))
+    cactus.height = Number(gpc_height.slice(0,gpc_height.length-2))-5
+    playerBox.width = 300;
+    playerBox.height = Number(gpc_height.slice(0,gpc_height.length-2))-5
+    function gameStart(){
+        player.create()
+        createCactus();
+    }
+           
+    function movePlayer(arrow){
+        Player_ctx.clearRect(player.x,player.y,player.width,player.height)
+        if(arrow === 'ArrowUp'){
+            if(player.y !== 0){
+                player.y = player.y-((playerBox.height-player.height)/2);
+            }
+        }
+        if(arrow === 'ArrowDown'){
+            if(player.y !== playerBox.height-player.height){
+                player.y = player.y+((playerBox.height-player.height)/2);
+            }
+        }
+        player.create()
+    }
+    
+    
+    function newCactusItem(x,y,width,height,img,eat){
+        let cactusItem = new Cactus(x,y,width,height,img,eat);
+        cactusArray.push(cactusItem)
+        cactusItem.create();
+    }
+
+    function createCactus(){
+        if(life===0){
+            mainBgm.pause();
+            // 웃는소리틀꺼
+            gameOverBox.classList.toggle('off')
+            Player_ctx.clearRect(player.x,player.y,player.width,player.height)
+            player.x=150;
+            player.y=0;
+            setTimeout(()=>{
+                gameEndBgm.play()
+            },500)
+            if(localStorage.getItem('bestScore')){
+                if(score > localStorage.getItem('bestScore')){
+                    localStorage.setItem('bestScore',score)
+                }
+            }else{
+                localStorage.setItem('bestScore',score)
+            }
+            return;
+        }
+        requestAnimationFrame(createCactus)
+        let randomIndex = Math.trunc(Math.random()*foodImg.length)
+        let randomFood = foodImg[randomIndex]
+        timer++;
+        if(timer%173 === 0){
+            newCactusItem(cactus.width,(playerBox.height-player.height),100,100,randomFood,String(randomFood.dataset.eat))
+        }
+        if(timer%200 === 0){
+            newCactusItem(cactus.width,(playerBox.height-player.height)/2,100,100,randomFood,String(randomFood.dataset.eat))
+        }
+        if(timer%123 === 0){
+            newCactusItem(cactus.width,0,100,100,randomFood,String(randomFood.dataset.eat))
+        }
+        moveCactus(cactusArray)
+    }
+
+    function lifeRemoveOne(){
+        life-=1;
+        lifeBox.textContent=`LIFE:${life}`
+    }
+    function RectOut(x,y,width,height,index){
+        Cac_ctx.clearRect(x,y,width,height);
+        cactusArray.splice(index,1)
+        //넘어갔을때 아픈소리내고 
+    }
+    //먹고 -> 다른거먹엇을때 음악이 시작중이면 pause하고 다시 play하는싯으로하자
+    function moveCactus(array=[]){
+        if(array.length!==0){
+            array.forEach((food,index) => {
+                let eatCase = (food.x <= player.x+player.width) && food.y === player.y//직선먹을때
+                // 조건을 묶을수있겟지만 일단 그러지말자 => 먹는케이스를 다시생각
+                if(eatCase){
+                    if(food.x+food.width < player.x === false){
+                        //캐릭터가 먹을시
+                        if(food.eat === 'false'){
+                            //비정상 음식먹음
+                            hurtBgm.load()
+                            hurtBgm.play()
+                            lifeRemoveOne()
+                        }
+                        else{
+                            //정상적으로 먹음
+                            eatBgm.load()
+                            eatBgm.play()
+                            score += 100;
+                            scoreBox.textContent=`SCORE:${score}`
+                            if(score % 1000 === 0){
+                                level+=1;
+                                levelUpBgm.play()
+                                levelBox.textContent=`LEVEL:${level}`
+                            }
+                        }
+                        RectOut(food.x,food.y,food.width,food.height,index);
+                    }
+                    else{
+                        //캐릭-음식 동일선상 근데 음식이 캐릭 넘어감
+                    Cac_ctx.clearRect(food.x,food.y,food.width,food.height,index);
+                    if(food.x < 0){
+                        if(food.eat==='true'){//그게음식일때
+                            lifeRemoveOne();
+                            missFoodBgm.play()
+                        }
+                        else{
+                            swingBgm.load();
+                            swingBgm.play();
+                        }
+                        RectOut(food.x,food.y,food.width,food.height,index);
+                        return;
+                    }
+                    food.x = food.x - speed - ((level-1)*2);
+                    food.create();
+                    }
+                }   
+                else if(food.x < 0){
+                    //그냥흘렷을시
+                    RectOut(food.x,food.y,food.width,food.height,index);
+                    if(food.eat === 'true'){
+                        lifeRemoveOne();
+                        missFoodBgm.load()
+                        missFoodBgm.play()
+                    }else{
+                        swingBgm.load()
+                        swingBgm.play()
+                    }
+                }
+                else{//그냥 움직이는상태
+                    Cac_ctx.clearRect(food.x,food.y,food.width,food.height,index);
+                    //장애물 속도 조절부분 => 위와 중복이지만 중간에 코드를 삽입해야되서 함수처리하진않겟다
+                    food.x = food.x - speed - ((level-1)*2);
+                    food.create();
+                }
+            })
+        }
+    }
+    gameStart()
+    //필살기같은것도 만들어서 음식다없어지게하는것도 잇으면 좋을듯?
+}
+
+
---
+++ main.js
@@ -0,0 +1,112 @@
+import {gamePlay} from './gamePlay.js'
+let mainBox = document.getElementById('game-main')
+let howPlayBox = document.getElementById('game-how')
+let howSwitch = 'true';
+let bestScore = localStorage.getItem('bestScore')?localStorage.getItem('bestScore'):0
+const audioBtn = document.getElementById('audio_play')
+const game_play_container = document.getElementById('game-play')
+const footerBox = document.getElementById('score-box')
+let mainBgm_range = document.getElementById('bgm-range')
+const backBtn = document.getElementById('back')
+let gameOverBox = document.getElementsByClassName('game-over')[0]
+
+const[mainBgm,eatBgm,swingBgm,hurtBgm,missFoodBgm,levelUpBgm,gameEndBgm] = document.querySelectorAll('audio')
+function renderView(container,template){
+    container.innerHTML = template
+}
+function backBtnEvent(){
+    const bestScoreBox = document.getElementById('best-score')
+    const bS = localStorage.getItem('bestScore')
+    //왜 ㅆ 다른박스는 수정되는데 베스트박스만안되냐고
+    //howTobox는 밑에 main이 render할때 만든다. 그러니까 애초에 박스를 찝어도 안찝히지 없으니까
+    //아우
+    mainBox.classList.toggle('off')
+    game_play_container.classList.toggle('off')
+    gameOverBox.classList.toggle('off')
+    footerBox.classList.toggle('off')
+    howPlayBox.classList.toggle('off')
+    audioBtn.setAttribute('aria-checked',"false")
+    audioBtn.classList.remove('play_off')
+    audioBtn.classList.add('play_on')
+    bestScoreBox.textContent = `Best:${bS}`
+}
+function startBtn_addEvent(){
+    const startBtn = document.getElementById('start')
+    startBtn.addEventListener('click',function(){
+        if(audioBtn.getAttribute('aria-checked') === 'true'){
+            mainBgm.play();
+        }
+        howPlayBox.classList.add('off')
+        game_play_container.classList.toggle('off')
+        footerBox.classList.toggle('off')
+        mainBox.classList.toggle('off');
+        gamePlay();
+    })
+}
+
+function audioBtnEvent(ele){
+    if(ele.getAttribute('aria-checked') === 'false'){
+        mainBgm.volume=0.5;
+        mainBgm.play();
+        mainBgm.loop = true
+        ele.setAttribute('aria-checked',"true")
+        ele.classList.remove('play_on')
+        ele.classList.add('play_off')
+    }
+    else if(ele.getAttribute('aria-checked') === 'true'){
+        mainBgm.pause();
+        ele.setAttribute('aria-checked',"false")
+        ele.classList.remove('play_off')
+        ele.classList.add('play_on')
+    }
+}
+function mainBgm_rangeEvent(value){
+    mainBgm.volume = value
+
+}
+howPlayBox.addEventListener('click',function(){
+        if(howSwitch === 'false'){
+            howSwitch='true'
+            renderView(mainBox,`
+            <div class="font game-main-title">Umm~ eating and Die</div>
+            <div class="game-main-content">
+                <div id="start" class="btn">Game start
+                </div>
+                <div id="best-score" class="btn">Best:${bestScore}</div>
+            </div>
+            `)
+            howPlayBox.textContent = 'How to Play'
+            startBtn_addEvent()
+        }else{
+            howSwitch='false';
+            renderView(mainBox,`
+            <div class="game-main-title">How To Play</div>
+            <div class="game-main-content how">
+                <div class="how">음식이 나오면 <span style="color:red">가려서</span> 먹으세요</div>
+                <div class="how">Life존재합니다. Score에따라 빨라집니다</div>
+                <div style="color:red" class="how">전체화면을 권장합니다.......</div>
+                <div style="color:red" class="how">소리나오니까 주의하시구요......</div>
+            </div>
+            `);
+            howPlayBox.textContent = 'B A C K'
+        }
+    })
+
+function mainView(){
+    audioBtn.addEventListener('click',(e)=>audioBtnEvent(e.target))
+    mainBgm_range.addEventListener('input',(e)=>mainBgm_rangeEvent(e.target.value))
+    renderView(mainBox,`
+    <div class="font game-main-title">Umm~ eating and Die</div>
+    <div class="game-main-content">
+        <div id="start" class="btn">Game start
+        </div>
+        <div id="best-score" class="btn">Best:${bestScore}</div>
+    </div>
+    `)
+    startBtn_addEvent()
+    backBtn.addEventListener('click',backBtnEvent)
+}
+
+mainView()
+//새로고침후 최초렌더시에 베스트스코어 0인거. => 하우투플레이를 클릭후 돌아올때만 bestScore받아와서 넣으니까
+
